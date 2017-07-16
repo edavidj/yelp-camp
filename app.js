4293dec4bae84f8295afdc93b49972ed4e48b2d2
Ethan Johnston <ethan.johnston9@hotmail.com>
2017-07-16 00:53:04 -0400

    added mongoose and mongo

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var path = require("path");
 var app = express();
 /**
  * Once done the tutorial look into adding features for receiving pictures from my imgur
  * AS well as uplading them to imgur (find a way to determine that it is yeri)
  */
+app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
@@ -14,7 +16,12 @@ app.get("/", function(req,res) {
 var campgrounds= [
         {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
         {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"}
+        {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
+        {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
+        {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
+        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
+        {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"}
        ] 
 app.get("/campgrounds", function(req, res) {   
    res.render("campgrounds", {campgrounds: campgrounds});
