518347f28fc4a2ea69b77b7184fe605c11fb4ee7
Ethan Johnston <ethan.johnston9@hotmail.com>
2017-07-23 22:19:35 -0400

    added basic mongodb features with mongoose

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var bodyParser = require("body-parser");
-var path = require("path");
-var app = express();
+var express     = require('express'),
+    bodyParser  = require("body-parser"),
+    path        = require("path"),
+    mongoose    = require("mongoose"),
+    app         = express();
 /**
  * Once done the tutorial look into adding features for receiving pictures from my imgur
  * AS well as uplading them to imgur (find a way to determine that it is yeri)
  */
+// mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://onetrueyeri:password@localhost:27017/admin", {useMongoClient: true}); //do more research into doing this a better way
 app.use(express.static(path.join(__dirname,'public')));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -13,32 +16,58 @@ app.set("view engine", "ejs");
 app.get("/", function(req,res) {
    res.render("landing");
 });
-var campgrounds= [
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
-        {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
-        {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg"},
-        {name: "Salmon Creek", image: "https://farm1.staticflickr.com/110/316612922_38fb0698f5.jpg"},
-        {name: "Granite Hill", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"}
-       ] 
+
+
+//schema setup
+
+var campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//   name: "Granite Hill", 
+//   image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"
+// }, function(err, campground){
+//   if(err){
+//       console.log(err);
+//   } else {
+//       console.log("NEWLY CREATED CAMPGROUND");
+//       console.log(campground);
+//   }
+// });
+
 app.get("/campgrounds", function(req, res) {   
-   res.render("campgrounds", {campgrounds: campgrounds});
+//    res.render("campgrounds", {campgrounds: campgrounds});
+  Campground.find({}, function(err, campgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds: campgrounds});
+    }
+  })
 });
 
 app.post("/campgrounds", function(req,res) {
-   //get data from form and add to campgrounds array
-   //redirect back to campgrounds page
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {name:name,image:image};
-   campgrounds.push(newCampground);
-   res.redirect("/campgrounds"); //defaults to get request
+  //get data from form and add to campgrounds array
+  //redirect back to campgrounds page
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name:name,image:image};
+  Campground.create(newCampground, function(err, campground){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(campground);
+    }
+  });
+  res.redirect("/campgrounds"); //defaults to get request
 });
 app.get("/campgrounds/new", function(req,res) {
-    res.render("new.ejs");
+  res.render("new.ejs");
 });
 app.listen(3000, function(){
-    console.log("The Yelp Camp server has started.");
+  console.log("The Yelp Camp server has started.");
 })
